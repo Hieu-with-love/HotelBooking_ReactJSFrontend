3b344674122dc1fcc4d5ec857d25f4d8724ce4d8
devzeus <hieu01bdvn@gmail.com>
2025-04-26 16:11:17 +0700

    change api url

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://hotelbookingspringbackend-production.up.railway.app";
+const API_URL = "https://hotelbooking-springbackend.onrender.com";
 
 export const api = axios.create({
     baseURL: API_URL,
